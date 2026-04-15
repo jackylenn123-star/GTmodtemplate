76e3577a1cbced4c77d0d8b961f9fda6befb0554
jackylenn123-star <jackylenn123@gmail.com>
2026-04-15 17:19:19 +0200

    Add files via upload

---
+++ Constants.cs
@@ -0,0 +1,8 @@
+namespace GTModTemplate;
+
+public static class Constants
+{
+    public const string Name = "**MODNAME**";
+    public const string Guid = "**GUID**";
+    public const string Version = "1.0.0";
+}
